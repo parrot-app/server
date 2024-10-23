06fca7863f53d77dd6cb97b40859a9cf650ac72d
Tarek Jellali <jellalitarek@gmail.com>
2024-10-23 23:57:35 +0200

    add: cleanup out of sync cached entries

--- src/handlers/Cache.handler.ts
+++ src/handlers/Cache.handler.ts
@@ -1,16 +1,21 @@
-import { Request, Response } from "express";
 import fs from 'fs-extra';
+import { Request, Response } from "express";
 import { AxiosResponse } from "axios";
+
 import { Config } from "../interfaces/Config.interface";
 import { CachedRequest } from "../interfaces/CachedRequest.interface";
 import { StoredCachedRequest } from "../interfaces/StoredCachedRequest.interface";
+import { logError, logIn } from '../helpers/Logger';
 
 export class CacheHandler {
+    private cachePath = '';
     constructor(
         private request: Request,
         private response: Response,
         private config: Config,
-    ) { }
+    ) {
+        this.cachePath = `${this.config.cachePath}/${this.config.requestsCacheFileName}`;
+    }
 
     public static init(config: Config) {
         const cachePath = `${config.cachePath}/${config.requestsCacheFileName}`;
@@ -21,21 +26,21 @@ export class CacheHandler {
     }
 
     public get cachedRequest(): CachedRequest | null {
-        const cachePath = `${this.config.cachePath}/${this.config.requestsCacheFileName}`;
-        const cache = JSON.parse(fs.readFileSync(cachePath, this.config.encoding));
-        const cachedRequest = this.config.matchBy(this.request, cache);
-        if (cachedRequest) {
-            return this.parseCachedResponse(cachedRequest);
+        const cache = JSON.parse(fs.readFileSync(this.cachePath, this.config.encoding)) as StoredCachedRequest[];
+        if (cache.length > 0) {
+            const cachedRequest = this.config.matchBy(this.request, cache);
+            if (cachedRequest) {
+                return this.parseCachedResponse(cachedRequest);
+            }
         }
         return null;
     }
 
     public saveCacheRequest(response: AxiosResponse) {
-        const cachePath = `${this.config.cachePath}/${this.config.requestsCacheFileName}`;
         const responseFilePath = this.createResponseBodyFile(response);
         const responseHeadersFile = this.createResponseHeadersFile(response);
 
-        const cache = JSON.parse(fs.readFileSync(cachePath, this.config.encoding));
+        const cache = JSON.parse(fs.readFileSync(this.cachePath, this.config.encoding));
         const newCache: CachedRequest[] = [
             ...cache,
             {
@@ -43,22 +48,26 @@ export class CacheHandler {
                 url: this.request.url,
                 body: this.request.body,
                 code: response.status,
-                headers: responseHeadersFile ? responseHeadersFile : undefined,
-                response: responseFilePath ? responseFilePath : undefined,
+                responseHeaders: responseHeadersFile ? responseHeadersFile : undefined,
+                responseBody: responseFilePath ? responseFilePath : undefined,
             },
         ];
-        fs.outputFileSync(cachePath, JSON.stringify(newCache, null, 4));
+        fs.outputFileSync(this.cachePath, JSON.stringify(newCache, null, 4));
     }
 
-    private parseCachedResponse(storedCachedRequest: StoredCachedRequest): CachedRequest {
+    private parseCachedResponse(storedCachedRequest: StoredCachedRequest): CachedRequest | null {
         let cachedRequest = {} as CachedRequest;
-        Object.assign(cachedRequest, storedCachedRequest)
+        Object.assign(cachedRequest, storedCachedRequest);
         if (
             storedCachedRequest.responseBody
             && typeof storedCachedRequest.responseBody === 'string'
-            && fs.existsSync(storedCachedRequest.responseBody)
         ) {
-            cachedRequest.responseBody = JSON.parse(fs.readFileSync(storedCachedRequest.responseBody, this.config.encoding));
+            if (fs.existsSync(storedCachedRequest.responseBody)) {
+                cachedRequest.responseBody = JSON.parse(fs.readFileSync(storedCachedRequest.responseBody, this.config.encoding));
+            } else {
+                this.cleanupRemovedEntries();
+                return null;
+            }
         }
         if (
             storedCachedRequest.responseHeaders
@@ -66,6 +75,9 @@ export class CacheHandler {
             && fs.existsSync(storedCachedRequest.responseHeaders)
         ) {
             cachedRequest.responseHeaders = JSON.parse(fs.readFileSync(storedCachedRequest.responseHeaders, this.config.encoding));
+        } else {
+            this.cleanupRemovedEntries();
+            return null;
         }
         return cachedRequest;
     }
@@ -86,5 +98,36 @@ export class CacheHandler {
         const filePath = `${this.config.cachePath}${this.request.url}_headers.json`
         fs.outputFileSync(filePath, JSON.stringify(response.headers, null, 4), this.config.encoding);
         return filePath;
+    }
+
+    private cleanupRemovedEntries() {
+        logError('Some entries seem out of sync withe the cache files, cleaning up!')
+        let cache = JSON.parse(fs.readFileSync(this.cachePath, this.config.encoding)) as StoredCachedRequest[];
+        if (cache.length > 0) {
+            cache = cache.filter(item => {
+                if (
+                    fs.existsSync(item.responseHeaders)
+                    && fs.existsSync(item.responseBody)
+                ) {
+                    return true;
+                } else if (
+                    !fs.existsSync(item.responseHeaders)
+                ) {
+                    if (fs.existsSync(item.responseBody)) {
+                        fs.removeSync(item.responseBody);
+                        return false;
+                    }
+                } else if (
+                    !fs.existsSync(item.responseBody)
+                ) {
+                    if (fs.existsSync(item.responseHeaders)) {
+                        fs.removeSync(item.responseHeaders);
+                        return false;
+                    }
+                }
+                return false;
+            });
+            fs.outputFileSync(this.cachePath, JSON.stringify(cache, null, 4));
+        }
     }
 }
