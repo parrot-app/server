0976f11543b6db733d3e2260ed87270552e08d28
Tarek Jellali <jellalitarek@gmail.com>
2024-11-19 22:07:22 +0100

    fix: log files with date and time

--- src/helpers/Logger.ts
+++ src/helpers/Logger.ts
@@ -1,12 +1,17 @@
 import winston from 'winston';
 import { ServerConfig } from './ParrotConfig';
 
+const currentDate = new Date(Date.now());
+
+const dateString = `${currentDate.getFullYear()}-${currentDate.getMonth()}-${currentDate.getDate()}T${currentDate.getHours()}-${currentDate.getMinutes()}-${currentDate.getSeconds()}`;
+
 export const logger = winston.createLogger({
   level: ServerConfig.logLevel,
   format: winston.format.json(),
   transports: [
     new winston.transports.File({
       dirname: ServerConfig.logPath,
+      filename: `parrot-session-${dateString}.log`
     }),
   ],
 });
